135ffa6bfd7ccf781cc693c98170c5c05a037fd6
Geraldo Felix Jr <118848329+GeraldoFelix@users.noreply.github.com>
2025-12-03 23:15:44 -0300

    Adding app.js
    adição da aplicação/main

---
+++ app.js
@@ -0,0 +1,14 @@
+import Binarytree from "./Binarytree.js";
+
+const tree = new Binarytree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(8);
+tree.insert(3);
+tree.insert(10);
+tree.insert(1);
+tree.insert(6);
+
+tree.inOrder();
+
+//console.log(tree);
